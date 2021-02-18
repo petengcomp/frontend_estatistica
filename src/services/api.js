13017012932a0b9425a3ddc521b09dc004c18832
HenriqueCruz <henriquepaulinocruz@hotmail.com>
2021-02-18 12:31:36 -0300

    removendo / a mais do baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://apiestatisticas.pet.inf.ufes.br/",
+  baseURL: "https://apiestatisticas.pet.inf.ufes.br",
 });
 
 export default api;
